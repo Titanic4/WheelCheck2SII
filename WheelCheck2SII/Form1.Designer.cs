42f2cdb6fa16734e4332ebb7cab4144c81fa40de
Titanic4 <tzmalenki@gmail.com>
2021-10-27 10:00:55 +0200

    Added deadzone detection with option to include it. By default the program will remove the deadzone from output file.

--- WheelCheck2SII/Form1.Designer.cs
+++ WheelCheck2SII/Form1.Designer.cs
@@ -28,21 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 334);
+            this.button1.Location = new System.Drawing.Point(12, 354);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(91, 23);
             this.button1.TabIndex = 0;
@@ -52,7 +55,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(598, 334);
+            this.button2.Location = new System.Drawing.Point(598, 354);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(104, 23);
             this.button2.TabIndex = 1;
@@ -62,14 +65,14 @@
             // 
             // chart1
             // 
-            chartArea2.AxisX.Minimum = 0D;
-            chartArea2.AxisX.Title = "Force";
-            chartArea2.AxisY.Title = "deltaXDeg";
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.Title = "Force";
+            chartArea1.AxisY.Title = "deltaXDeg";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.Cursor = System.Windows.Forms.Cursors.Arrow;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
             this.chart1.Size = new System.Drawing.Size(690, 300);
@@ -79,39 +82,61 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(213, 318);
+            this.label1.Location = new System.Drawing.Point(213, 338);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.Size = new System.Drawing.Size(130, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "WheelCheck2SII is ready.";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(457, 334);
+            this.numericUpDown1.Location = new System.Drawing.Point(436, 354);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(426, 318);
+            this.label2.Location = new System.Drawing.Point(413, 338);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(164, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Desired precision(decimal places)";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(436, 318);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(201, 18);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Keep deadzone(not recommended)";
+            this.checkBox1.UseCompatibleTextRendering = true;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(714, 369);
+            this.ClientSize = new System.Drawing.Size(714, 395);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
@@ -137,6 +162,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- WheelCheck2SII/Form1.cs
+++ WheelCheck2SII/Form1.cs
@@ -19,7 +19,12 @@ namespace WheelCheck2SII
         string[] CSV;
         string[] CSVData;
         double max;
+        bool dzfound = false;
+        int dzoffset = 0;
         int count = 0;
+        int noforce = 0;
+        decimal deadzone = 0;
+        
         List<WheelCheckCSV> wheelCheck = new List<WheelCheckCSV>();
         OpenFileDialog OpenDialog = new OpenFileDialog();
         SaveFileDialog SaveDialog = new SaveFileDialog();
@@ -27,6 +32,20 @@ namespace WheelCheck2SII
         {
            
             InitializeComponent();
+            numericUpDown1.Enabled = false;
+      
+
+            // Set up the delays for the ToolTip.
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            // Force the ToolTip text to be displayed whether or not the form is active.
+            toolTip1.ShowAlways = true;
+            toolTip1.SetToolTip(button1, "Press this to select CSV file");
+            toolTip1.SetToolTip(button2, "Press this to save the generated LUT file");
+            toolTip1.SetToolTip(checkBox1, "This makes the program add deadzone to output LUT file.\nIt's recommended to keep this off.");
+            toolTip1.SetToolTip(numericUpDown1, "This sets the desired precision of given values.\nLower values make the LUT more coarse, higher will make it finer. ");
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -34,9 +53,13 @@ namespace WheelCheck2SII
 
             OpenDialog.ShowHelp = false;
             OpenDialog.AddExtension = true;
+            noforce = 0;
+            dzfound = false;
+            dzoffset = 0;
             OpenDialog.DefaultExt = ".csv";
             OpenDialog.Title = "Select WheelCheck CSV";
-            if (OpenDialog.ShowDialog() == DialogResult.OK)
+            DialogResult Res = OpenDialog.ShowDialog();
+            if (Res == DialogResult.OK)
             {
                 string Path = OpenDialog.FileName;
                 if (Path.Contains("csv"))
@@ -89,24 +112,59 @@ namespace WheelCheck2SII
 
                             }
 
-                            for (int i = 0; i < wheelCheck.Count(); i++)
-                                chart1.Series["deltaXDeg"].Points.AddXY(i, wheelCheck[i].deltaXDeg);
-                        }
+                            for (int i = 0; i < wheelCheck.Count() - 1; i++) {
+                                if (wheelCheck[i].deltaXDeg < 0.01) { 
+                                   noforce++;
+                                }
+                                else
+                                {
+                                    if (!dzfound)
+                                    {
+                                        dzfound = true;
+                                        dzoffset = i;
+                                    }
+                                }
+                                    chart1.Series["deltaXDeg"].Points.AddXY(i, wheelCheck[i].deltaXDeg);
+                            } }
 
-                        chart1.Update();
                         DataPoint maxDataPoint = chart1.Series["deltaXDeg"].Points.FindMaxByValue();
-
+                        numericUpDown1.Enabled = true;
                         count = chart1.Series["deltaXDeg"].Points.Count;
                         max = maxDataPoint.YValues[0];
-                        MessageBox.Show($"Successfully loaded {count} values from selected CSV file.");
-                        label1.Text = $"Amount of points: {chart1.Series["deltaXDeg"].Points.Count}\nMaximum: {max}";
+
+                        deadzone = (decimal)noforce / count;
+                        label1.Text = $"Amount of points: {chart1.Series["deltaXDeg"].Points.Count}\nMaximum: {Math.Round(max, 2)}\nDeadzone: {deadzone*100} %";
+                        if (chart1.Series.FindByName("outvals") != null)
+                        {
+                            chart1.Series["outvals"].Points.Clear();
+                        }
+
+                        if (chart1.Series.FindByName("outvals") == null)
+                        {
+
+                            chart1.Series.Add("outvals");
+                            chart1.Series["outvals"].ChartType = SeriesChartType.Line;
+                            chart1.Series["outvals"].BorderWidth = 2;
+                        }
+
+
+                        for (int i = 0; i < wheelCheck.Count(); i++)
+                        {
+                            
+                            chart1.Series["outvals"].Points.AddXY(i, Math.Round(wheelCheck[i].deltaXDeg / max, (int)numericUpDown1.Value) * max);
+                        }
+                        chart1.Update();
+                        MessageBox.Show($"Successfully loaded {count} values from selected CSV file.\n Detected deadzone:{deadzone * 100}%");
                     }
 
                 }
             }
             else
             {
-                MessageBox.Show("The selected file isn't made by WheelCheck.");
+                if (Res != DialogResult.Cancel)
+                {
+                    MessageBox.Show("The selected file isn't made by WheelCheck.");
+                }
             }
             }
 
@@ -118,9 +176,26 @@ namespace WheelCheck2SII
                 var header = "SiiNunit\r\n{\r\ninput_force_feedback_lut : ffb.lut {\n# This ffb_lut.sii file was generated by WheelCheck2SII converter.\n\n";
                 var footer = "\n}\r\n}";
                 var contents = "";
+                noforce = 0;
+       
                 foreach (var item in wheelCheck)
                 {
-                    contents += $"output_values[]: {Math.Round(item.deltaXDeg / max, (int)numericUpDown1.Value).ToString("0.##################", CultureInfo.GetCultureInfo("en-GB"))}\n";
+                    if(Math.Round(item.deltaXDeg / max, (int)numericUpDown1.Value) < 0.09)
+                    {
+                        if (checkBox1.Checked)
+                        {
+                            contents += $"output_values[]: {Math.Round(item.deltaXDeg / max, (int)numericUpDown1.Value).ToString("0.##################", CultureInfo.GetCultureInfo("en-GB"))}\n";
+                        }
+                        continue;
+                    }
+                    if (Math.Round(item.deltaXDeg / max, (int)numericUpDown1.Value) > 1)
+                    {
+                        contents += $"output_values[]: 1\n";
+                    }
+                    else
+                    {
+                        contents += $"output_values[]: {Math.Round(item.deltaXDeg / max, (int)numericUpDown1.Value).ToString("0.##################", CultureInfo.GetCultureInfo("en-GB"))}\n";
+                    }
                 }
                 var fileContents = header + contents + footer;
                 SaveDialog.FileName = "ffb_lut.sii";
@@ -131,11 +206,12 @@ namespace WheelCheck2SII
                     try
                     {
                         File.WriteAllText(SaveDialog.FileName, fileContents);
-                        MessageBox.Show($"Successfully saved ffb_lut.sii file in {SaveDialog.FileName}.\n");
+                   
+                        MessageBox.Show($"Successfully saved ffb_lut.sii file in {SaveDialog.FileName}");
                     }
                     catch (Exception E)
                     {
-                        MessageBox.Show($"There was problem saving the file. Either you don't have permission, or the drive you want to write into is read only.\n{E.Message}");
+                        MessageBox.Show($"There was problem saving the file. Either you don't have permission, or the drive you want to write into is read only.\n{E.Message}\n{E.StackTrace}");
                     }
 
                 }
